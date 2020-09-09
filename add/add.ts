632adb2713c4c96432b4c16ed75845497f343135
Bradley Lacke <thedodus@gmail.com>
2020-09-09 08:38:54 -0400

    completed add.ts exercise

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,13 @@
-function add(param1: number, param2: number): number {
-
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-// function add2(param1: number[]): number {
+function add2(...numbers) {
+    return numbers.reduce((prevVal, currentVal) => prevVal + currentVal);
+}
+console.log(add(1, 2));
+console.log(add(3, 2));
 
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
-
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
+console.log(add2(1,2,3,4,5));
+console.log(add2(2,3));
 
