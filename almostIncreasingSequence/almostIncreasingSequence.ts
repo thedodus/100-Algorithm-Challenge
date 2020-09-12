b42feba28bc3936e12d5d1cb4dc4402f611830a3
Bradley Lacke <thedodus@gmail.com>
2020-09-12 08:23:20 -0400

    final solution to alphabeticshift

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,13 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+    let count = 0;
 
+    for (let i = 0; i < sequence.length; i++){
+        if ( sequence[i + 1] < sequence[i]){
+            count++;
+        }
+    }
+    return count <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+console.log(almostIncreasingSequence([1, 3, 2])) 
