78aeaa5031b8656ac5ce2d8130ad004e3849ea7e
Bradley Lacke <thedodus@gmail.com>
2020-09-12 12:51:49 -0400

    completed solutions

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,22 @@
 function alphabetSubsequence(s: string): boolean {
+    const chars: string[] = s.split("");
+    const charValues = [];
+    
+    chars.forEach((char) => {
+        charValues.push(char.charCodeAt(0));
+    });
 
+    if(new Set(charValues).size !== charValues.length){
+        return false;
+    }
+
+    for(let i = 0; i < charValues.length; i++){
+        if(charValues[i] >= charValues[i + 1]){
+            return false;
+        }
+    }
+    
+    return true;
 }
 
 console.log(alphabetSubsequence('zab'))
--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 function alternatingSums(a: number[]): number[] {
+    let teamOne = 0;
+    let teamTwo = 0;
 
+    a.forEach((element, index) => {
+        if(index % 2 === 0){
+            teamOne += element;
+        } else {
+            teamTwo += element;
+        }
+    })
+
+    return [teamOne, teamTwo];
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
